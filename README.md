5408635279a2506df317d05ad73742f090513313
1nterfac3 <nibznortex@gmail.com>
2024-06-04 12:11:45 +0100

    Minor Ugrades

---
+++ README.md
@@ -0,0 +1,9 @@
+#Simple Dictionary
+
+This application uses the Free Dictionary API to find the meaning of words, synonyms, antonyms and the likes
+
+Link: https://dictionaryapi.dev/ 
+
+There will be a second version of this at a later date.
+
+The screen will be split in half this time. One part blue and the other part for displaying the search results
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+//Capitalizing the First letter of a string
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 //Fetching data from the dictionary API
-
 async function fetchData() {
     //*Always use try and catch for this type of function
     try {
@@ -12,6 +15,8 @@ async function fetchData() {
         const definition = document.getElementById("definition");
         const synonyms = document.getElementById("synonym");
         const antonyms = document.getElementById("antonym");
+
+        //TODO: Create the list variables to store the JSON Data in
         
 
         //*Fetching data from the API
@@ -23,15 +28,23 @@ async function fetchData() {
             throw new Error("Couldn't fetch the resource");
         }
 
+        //Array of the elements with the class name "output"
+        const resultSection = document.getElementsByClassName("output"); 
+        
+        //* Change the display of the elements in the output class from none to inline-block
+        for (let i=0; i<resultSection.length; i++) {
+            resultSection[i].style.display = "inline-block";
+        }
+
         //TODO: Load the synonyms,antonyms,phonetics and other definitions from the API (use loops and array methods)
         const data = await response.json();
-        wordOutput.innerHTML = `Word: " ${data[0].word} "`;
+        wordOutput.innerHTML = capitalizeFirstLetter(data[0].word);
         definition.innerHTML = `Definition: ${data[0].meanings[0].definitions[0].definition}`;
 
 
         console.log(data[0]); //Always log the data to inspect the JSON
     }
     catch(error) {
-        console.log(error);
+        console.log(error); //Catch the errors present
     }
 }
