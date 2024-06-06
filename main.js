d5528ac02c5243ba18863801b4884865e39c651e
Ikubie <40043276+1nterfac3@users.noreply.github.com>
2024-06-06 02:57:32 +0100

    Current Version of Files

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Capitalizing the First letter of a string
 const capitalizeFirstLetter = (string) => {
+    //Capitalizing the first letter and adding it back to the original string minus the first letter
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
@@ -39,7 +40,59 @@ async function fetchData() {
         //TODO: Load the synonyms,antonyms,phonetics and other definitions from the API (use loops and array methods)
         const data = await response.json();
         wordOutput.innerHTML = capitalizeFirstLetter(data[0].word);
-        definition.innerHTML = `Definition: ${data[0].meanings[0].definitions[0].definition}`;
+
+        //Definitions
+        const allMeanings = data[0].meanings;
+        const definitionsList =[];
+        
+        for (let i=0; i<allMeanings.length; i++) {
+            const partOfSpeech = allMeanings[i].partOfSpeech;
+            const temp = `${capitalizeFirstLetter(partOfSpeech)}: ${allMeanings[i].definitions[0].definition}`;
+            definitionsList.push(temp);
+        }
+        definition.innerHTML = definitionsList.join('  ');
+
+        //Phonetics
+        const phonetics = data[0].phonetics;
+        const phoneticsList = [];
+        for (let i=0; i<phonetics.length; i++) {
+            phoneticsList.push(phonetics[i].text);
+        }
+        phonetic.innerHTML = phoneticsList.join('  ');
+
+        //? Why is this side undefined but antonyms isn't?
+        const allSynonyms = data[0].meanings[0].synonyms; //The Synonyms of the particular word (array)
+        console.log(allSynonyms);
+        const allSynonymsList = [];
+        //TODO: Use a nested for loop to try and solve the issue of the synonyms being undefined
+        for (let i=0; i<allMeanings.length-1; i++) {
+            const item = allSynonyms[i]; //Index of the synonyms array
+            console.log(item);
+
+            if (allSynonyms === undefined) {
+                synonyms.innerHTML = "N/A"; //No synonyms for the word
+            } else {
+                allSynonymsList.push(item); //Add Synonyms to the list
+            }  
+        }
+        synonyms.innerHTML = allSynonymsList.join(' , '); //Display Synonyms
+
+
+        //Antonyms
+        const allAntonyms = data[0].meanings[0].antonyms; //The Antonyms of the particular word (array)
+        const allAntonymsList = [];
+        // console.log(allAntonyms);
+
+        for (let i=0; i<allMeanings.length-1; i++) {
+            const item = allAntonyms[i]; //Index of the antonyms array
+
+            if (allAntonyms.length === 0) {
+                antonyms.innerHTML = "No Antonyms"; //No antonyms for the word
+            } else {
+                allAntonymsList.push(item); //Add antonyms to the list
+            }
+        }
+        antonyms.innerHTML = allAntonymsList.join(' , '); //Display the antonyms
 
 
         console.log(data[0]); //Always log the data to inspect the JSON
